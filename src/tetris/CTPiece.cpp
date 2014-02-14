5e0ec52b90dbd97abb911fcf1a8a942711fd96b9
Antoine Pinsard <antoine@Fudge>
2014-02-14 16:48:39 +0100

    Ajout de la classe CTPiece

---
+++ src/tetris/CTPiece.cpp
@@ -0,0 +1,17 @@
+
+#include "CTPiece.h"
+
+CTPiece::CTPiece(int iX, int iY, const CVector3& color) :
+    CPieceAbstract(3, iX, iY, color)
+{
+
+  TPieceRow row;
+
+  row[0] = 1; row[1] = 1; row[2] = 1;
+  this->m_table[2] = row;
+  row[0] = 0; row[1] = 1; row[2] = 0;
+  this->m_table[1] = row;
+  row[0] = 0; row[1] = 1; row[2] = 0;
+  this->m_table[0] = row;
+
+}
---
+++ src/tetris/CTPiece.h
@@ -0,0 +1,13 @@
+
+#ifndef __CTPIECE_H__
+#define __CTPIECE_H__
+
+#include "CPieceAbstract.h"
+
+class CTPiece : public CPieceAbstract {
+
+  public:
+    CTPiece(int iX, int iY, const CVector3& color);
+};
+
+#endif
