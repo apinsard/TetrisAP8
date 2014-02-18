dfcf8fc940f937adfb36f1f83df4984e0e9b2bec
Antoine Pinsard <antoine.pinsard@gmail.com>
2014-02-18 15:58:35 +0100

    Révision de l'arborescence des tests

--- src/tetris/CProjetTetris.cpp
+++ src/tetris/CProjetTetris.cpp
@@ -14,19 +14,19 @@
 
 CProjetTetris::CProjetTetris() {
 
-	m_game = CTetrisGame(POSX_BOARD, POSY_BOARD, WIDTH_BOARD, HEIGHT_BOARD, DIM_CASE);
-	m_step = 0;
+  m_game = CTetrisGame(POSX_BOARD, POSY_BOARD, WIDTH_BOARD, HEIGHT_BOARD, DIM_CASE);
+  m_step = 0;
 
-	// attributs rajoutés pour montrer un exemple de pièce qui descend
-	m_posYPiece = HEIGHT_BOARD;
-	m_posXPiece = WIDTH_BOARD/2;
+  // attributs rajoutés pour montrer un exemple de pièce qui descend
+  m_posYPiece = HEIGHT_BOARD;
+  m_posXPiece = WIDTH_BOARD/2;
 }
 
 /****************************************/
 
 CProjetTetris::~CProjetTetris() {
 
-	cout << "Destruction objet CProjetTetris" << endl;
+  cout << "Destruction objet CProjetTetris" << endl;
 }
 
 //----------------------------------------------------------
@@ -35,7 +35,7 @@ CProjetTetris::~CProjetTetris() {
 //----------------------------------------------------------
 bool CProjetTetris::OnInit() {
 
-	return true;
+  return true;
 }
 
 //----------------------------------------------------------
@@ -44,45 +44,45 @@ bool CProjetTetris::OnInit() {
 //----------------------------------------------------------
 void CProjetTetris::OnRender() {
 
-	//-------------------------------------------------------
-	// gestion du temps
-	//-------------------------------------------------------
-	if ( m_step> 50 ) 
-	{
-		m_step = 0; 
-		m_posYPiece--; // à chaque pas de temps, on fait évoluer la coordonnée en y de la pièce 
-	}
+  //-------------------------------------------------------
+  // gestion du temps
+  //-------------------------------------------------------
+  if ( m_step> 50 ) 
+  {
+    m_step = 0; 
+    m_posYPiece--; // à chaque pas de temps, on fait évoluer la coordonnée en y de la pièce 
+  }
 
-	//-------------------------------------------------------
-	// commande utilisateur
-	//-------------------------------------------------------
-	if(m_UserEvents->m_keyBoardEvents[Key_a])cout << "la touche 'a' a bien été utilisée" << endl;
-	if(m_UserEvents->m_keyBoardEvents[Key_z])cout << "la touche 'z' a bien été utilisée" << endl;
-	if(m_UserEvents->m_keyBoardEvents[Key_DOWN])cout << "la touche flèche bas a bien été utilisée" << endl;
-	if(m_UserEvents->m_keyBoardEvents[Key_RIGHT])cout << "la touche flèche droite a bien été utilisée" << endl;
-	if(m_UserEvents->m_keyBoardEvents[Key_LEFT])cout << "la touche flèche gauche a bien été utilisée" << endl;
-	if(m_UserEvents->m_keyBoardEvents[Key_SPACE])cout << "la touche espace a bien été utilisée" << endl;
-	
-	//-------------------------------------------------------
-	// affichage
-	//-------------------------------------------------------
-	// on passe en rendu 2d
-	m_renderer.Start2DRender();
+  //-------------------------------------------------------
+  // commande utilisateur
+  //-------------------------------------------------------
+  if(m_UserEvents->m_keyBoardEvents[Key_a])cout << "la touche 'a' a bien été utilisée" << endl;
+  if(m_UserEvents->m_keyBoardEvents[Key_z])cout << "la touche 'z' a bien été utilisée" << endl;
+  if(m_UserEvents->m_keyBoardEvents[Key_DOWN])cout << "la touche flèche bas a bien été utilisée" << endl;
+  if(m_UserEvents->m_keyBoardEvents[Key_RIGHT])cout << "la touche flèche droite a bien été utilisée" << endl;
+  if(m_UserEvents->m_keyBoardEvents[Key_LEFT])cout << "la touche flèche gauche a bien été utilisée" << endl;
+  if(m_UserEvents->m_keyBoardEvents[Key_SPACE])cout << "la touche espace a bien été utilisée" << endl;
 
-	// affichage du score
-	DrawInfo();
+  //-------------------------------------------------------
+  // affichage
+  //-------------------------------------------------------
+  // on passe en rendu 2d
+  m_renderer.Start2DRender();
 
-	// affichage du tetris
-	DrawTetris();
+  // affichage du score
+  DrawInfo();
 
-	// ICI AFFICHAGE DE MON CARRE DE TEST
-	DrawFillRect ( m_game.GetXPos() + m_posXPiece*m_game.GetCaseDim(), m_game.GetYPos()+ m_posYPiece*m_game.GetCaseDim(), m_game.GetCaseDim(), m_game.GetCaseDim(), CVector3(255.0f/255.0f,153.0f/255.0f,153.0f/255.0f) );
+  // affichage du tetris
+  DrawTetris();
 
-	// fin du rendu 2d
-	m_renderer.End2DRender();
+  // ICI AFFICHAGE DE MON CARRE DE TEST
+  DrawFillRect ( m_game.GetXPos() + m_posXPiece*m_game.GetCaseDim(), m_game.GetYPos()+ m_posYPiece*m_game.GetCaseDim(), m_game.GetCaseDim(), m_game.GetCaseDim(), CVector3(255.0f/255.0f,153.0f/255.0f,153.0f/255.0f) );
 
-	// maj du compteur de passage dans cette fonction
-	m_step ++;
+  // fin du rendu 2d
+  m_renderer.End2DRender();
+
+  // maj du compteur de passage dans cette fonction
+  m_step ++;
 }
 
 //----------------------------------------------------------
@@ -91,7 +91,7 @@ void CProjetTetris::OnRender() {
 //----------------------------------------------------------
 void CProjetTetris::OnRelease() {
 
-	// Rien a liberer
+  // Rien a liberer
 }	
 
 //----------------------------------------------------------
@@ -99,34 +99,34 @@ void CProjetTetris::OnRelease() {
 //----------------------------------------------------------
 void CProjetTetris::DrawInfo() 
 {
-	std::string title = Helpers::ToString("Mon Tetris");
-	std::string scoreStr = "Score : " + Helpers::ToString(200000);
-	
-	m_renderer.DrawText(title, 300, 500, CRenderer::TS_18, CVector3(1.0f, 1.0f, 1.0f));
-	m_renderer.DrawText(scoreStr, 500, 160, CRenderer::TS_12, CVector3(0.0f, 1.0f, 0.0f));	
+  std::string title = Helpers::ToString("Mon Tetris");
+  std::string scoreStr = "Score : " + Helpers::ToString(200000);
+
+  m_renderer.DrawText(title, 300, 500, CRenderer::TS_18, CVector3(1.0f, 1.0f, 1.0f));
+  m_renderer.DrawText(scoreStr, 500, 160, CRenderer::TS_12, CVector3(0.0f, 1.0f, 0.0f));	
 }
 
 /****************************************/
 
 void CProjetTetris::DrawTetris() {
 
-	vector<TGameRow> gameTable = m_game.GetBoard().GetGameTable();
-	float	tetrisWidth = gameTable[0].size()* m_game.GetCaseDim();
-	float	tetrisHeight = gameTable.size() * m_game.GetCaseDim();
+  vector<TGameRow> gameTable = m_game.GetBoard().GetGameTable();
+  float	tetrisWidth = gameTable[0].size()* m_game.GetCaseDim();
+  float	tetrisHeight = gameTable.size() * m_game.GetCaseDim();
 
-	// fond du tetris
-	DrawFillRect ( m_game.GetXPos(), m_game.GetYPos(), tetrisWidth, tetrisHeight, CVector3(153.0f/255.0f,153.0f/255.0f,153.0f/255.0f) );
-	//DrawFillRect ( m_game.GetXPos(), m_game.GetYPos(), tetrisWidth, tetrisHeight, CVector3(17.0f/255.0f,218.0f/255.0f,84.0f/255.0f) );
-	
-	// affichage du "cadre" tetris
-	DrawRect ( m_game.GetXPos()-0.1f, m_game.GetYPos()-0.1f, (tetrisWidth+0.2f), (tetrisHeight+0.2f), CVector3(213.0f/255.0f,213.0f/255.0f,213.0f/255.0f) );
+  // fond du tetris
+  DrawFillRect ( m_game.GetXPos(), m_game.GetYPos(), tetrisWidth, tetrisHeight, CVector3(153.0f/255.0f,153.0f/255.0f,153.0f/255.0f) );
+  //DrawFillRect ( m_game.GetXPos(), m_game.GetYPos(), tetrisWidth, tetrisHeight, CVector3(17.0f/255.0f,218.0f/255.0f,84.0f/255.0f) );
 
-	
-	
-	// affichage du tableau tetris
+  // affichage du "cadre" tetris
+  DrawRect ( m_game.GetXPos()-0.1f, m_game.GetYPos()-0.1f, (tetrisWidth+0.2f), (tetrisHeight+0.2f), CVector3(213.0f/255.0f,213.0f/255.0f,213.0f/255.0f) );
 
 
-	// affichage de la pîece en cours
+
+  // affichage du tableau tetris
+
+
+  // affichage de la pîece en cours
 
 }
 
@@ -135,42 +135,42 @@ void CProjetTetris::DrawTetris() {
 //----------------------------------------------------------
 void CProjetTetris::DrawFillRect(int x, int y, float w, float h, const CVector3 &color) {
 
-	glColor3f(color.x,color.y,color.z);
-	glBegin(GL_QUADS);
-	glVertex2i(x, y);
-	glVertex2i(x,  y+h);
-	glVertex2i(x+w,  y+h);
-	glVertex2i(x+w, y);
-    glEnd();
+  glColor3f(color.x,color.y,color.z);
+  glBegin(GL_QUADS);
+  glVertex2i(x, y);
+  glVertex2i(x,  y+h);
+  glVertex2i(x+w,  y+h);
+  glVertex2i(x+w, y);
+  glEnd();
 }
 
 /****************************************/
 
 void CProjetTetris::DrawRect(int x, int y, float w, float h, const CVector3 &color, float lineWidth)  {
 
-	glColor3f(color.x,color.y,color.z);
-	glLineWidth(lineWidth);
-	glBegin(GL_LINES);
-	glVertex2f(x, y);
-	glVertex2f(x,  y+h);
-	glVertex2f(x,  y+h);
-	glVertex2f(x+w,  y+h);
-	glVertex2f(x+w,  y+h);
-	glVertex2f(x+w, y);
-	glVertex2f(x+w, y);
-	glVertex2f(x, y);
-	glEnd();
+  glColor3f(color.x,color.y,color.z);
+  glLineWidth(lineWidth);
+  glBegin(GL_LINES);
+  glVertex2f(x, y);
+  glVertex2f(x,  y+h);
+  glVertex2f(x,  y+h);
+  glVertex2f(x+w,  y+h);
+  glVertex2f(x+w,  y+h);
+  glVertex2f(x+w, y);
+  glVertex2f(x+w, y);
+  glVertex2f(x, y);
+  glEnd();
 }
 
 /****************************************/
 
 void CProjetTetris::DrawFillSquare(int x, int y, float dim, const CVector3 &color)  {
 
-	glColor3f(color.x,color.y,color.z);
-	glBegin(GL_QUADS);
-	glVertex2f(x, y);
-	glVertex2f(x,  y+dim);
-	glVertex2f(x+dim,  y+dim);
-	glVertex2f(x+dim, y);
-	glEnd();
+  glColor3f(color.x,color.y,color.z);
+  glBegin(GL_QUADS);
+  glVertex2f(x, y);
+  glVertex2f(x,  y+dim);
+  glVertex2f(x+dim,  y+dim);
+  glVertex2f(x+dim, y);
+  glEnd();
 }
